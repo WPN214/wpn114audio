1f29ea1ea453982c0e5106a5f1359477b50b6add
Pierre Cochard <pchd@quarre.fr>
2019-08-15 11:31:12 +0200

    travis

--- tools/travis/dependencies.sh
+++ tools/travis/dependencies.sh
@@ -6,8 +6,9 @@
 		sudo apt-get install wget 
 		wget -nv https://cmake.org/files/v3.14/cmake-3.14.3-Linux-x86_64.tar.gz -O cmake-linux.tgz
 
+    	sudo add-apt-repository --yes ppa:beineri/opt-qt-5.12.3-xenial
 		sudo apt-get install -qq --force-yes \
-			libasound-dev jack2 portaudio19-dev \
+			libasound-dev jackd2 portaudio19-dev \
 			libgl1-mesa-dev \
 			qt513base qt512quickcontrols2 
 
